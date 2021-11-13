3f8ec2c952c4c603d9d3bd4ef272071f893442ad
Roey Borsteinas <roeyb1@hotmail.com>
2021-11-13 16:55:40 -0500

    Laid the groundwork for the revised serial protocol

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <Arduino.h>
+#include <Serial/Serial.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup() 
+{
+    SerialAPI::init('a', 9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+    if (SerialAPI::update())
+    {
+        char data[256];
+        memset(data, 0, 256);
+
+        if (SerialAPI::read_data(data, 256) != -1)
+        {
+            Serial.println(data);
+        }
+        const char* send_data = "Hello world from the MCU";
+        const size_t len = strlen(send_data);
+        SerialAPI::send_bytes('d', send_data, len);
+    }
+
+    {
+    }
+
+    delay(10);
 }
