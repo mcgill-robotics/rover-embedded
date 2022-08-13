bbc92198281fba08488d11dc06df33aaa4e8b4ef
Sam Dayo <56176274+samdayo2019@users.noreply.github.com>
2022-08-13 19:35:26 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/watchdog.h"
 #include <Rover_Limits.h>
+#include <Rover_SerialAPI.h>
 
 #define HIGHEST_ANGLE_VOLTAGE 3850
 #define ZERO_POINT 200
@@ -56,9 +57,13 @@ typedef struct{
 FloatBuffer fbuf; 
 static char buffer[SERIAL_RX_BUFFER_SIZE];
 static float fbuffer[SERIAL_RX_BUFFER_SIZE];
+char temp_msg[25];
 
 float positions[5];
-char buf[21]; 
+char buf[25];
+float * clawState;
+float tempClaw;
+bool accClawState = false;
 //TODO: add array to contain limit switch states 
 volatile float wr_position; 
 volatile float wp_position; 
@@ -83,7 +88,7 @@ void ConnectionLostISR();
 void setup() {
   // put your setup code here, to run once:
 
-  Serial.begin(9600);
+  SerialAPI::init('0', 9600);
   
   Wrist_Roll.begin(aggKp, aggKi, aggKd, regKp, regKi, regKd);
   Wrist_Pitch.begin(elbaggKp, elbaggKi, elbaggKd, elbregKp, elbregKi, elbregKd);
@@ -111,177 +116,24 @@ void setup() {
   // get the hardware averager to work!
   // ADCHardwareOversampleConfigure(ADC0_BASE, 64);
 
-  // Set up watchdog timer
-  SysCtlPeripheralEnable(SYSCTL_PERIPH_WDOG0);
+  // // Set up watchdog timer
+  // SysCtlPeripheralEnable(SYSCTL_PERIPH_WDOG0);
 
-  // Set watchdog timer to 10 ms
-  WatchdogReloadSet(WATCHDOG0_BASE, SysCtlClockGet() / 100);
+  // // Set watchdog timer to 10 ms
+  // WatchdogReloadSet(WATCHDOG0_BASE, SysCtlClockGet() / 100);
   
-  // Attach ISR to watchdog
-  WatchdogIntRegister(WATCHDOG0_BASE, &ConnectionLostISR);
+  // // Attach ISR to watchdog
+  // WatchdogIntRegister(WATCHDOG0_BASE, &ConnectionLostISR);
 
-  // Enable watchdog
-  WatchdogEnable(WATCHDOG0_BASE);
+  // // Enable watchdog
+  // WatchdogEnable(WATCHDOG0_BASE);
+  temp_msg[0] = '0';
+  float stff[6] = {0.0};
+  memcpy(temp_msg+1,stff,24);
 
 }
 
 void loop() {
-
-  // label:
-  //    Serial.print( Wrist_Pitch encoder: ");Serial.println Wrist_Pitch.getCurrentAngle());
-  //    Serial.print( Wrist_Roll encoder: ");Serial.println Wrist_Roll.getCurrentAngle());
-  //    Serial.println();
-  //    delay(1000);
-  // goto label;
-
-  // Get new setpoint from serial - will be gone when the serial bridge is up
-  // if(Serial.available()){
-  //   Serial.println("Press A to change setpoint for wrist pitch");
-  //   Serial.println("Press S to see encoder position for wrist pitch");
-  //   Serial.println("Press D to change setpoint for wrist roll");
-  //   Serial.println("Press F to see the encoder position for wrist roll");
-  //   Serial.println("Press G to change setpoint for shoulder");
-  //   Serial.println("Press H to see the encoder position for shoulder");
-  //   Serial.println("Press J to change setpoint for elbow");
-  //   Serial.println("Press K to see the encoder position for elbow");
-  //   Serial.println("Press W to change setpoint for waist");
-  //   Serial.println("Press E to see the encoder position for waist");
-
-  //   if(Serial.available()){
-
-  //     bool valid_input = false;
-
-  //     while(!valid_input){
-  //       if(Serial.available()){
-  //         int input_character = Serial.read();
-
-  //         switch(input_character){
-  //           case 'A':
-  //             Serial.print("Enter new setpoint for wrist pitch: ");
-  //             while(true){
-  //               if(Serial.available()){
-  //                 double set = (double) Serial.parseFloat();
-  //               Wrist_Pitch.newSetpoint(set); 
-  //                 delay(1000);
-  //                 break;
-  //               }
-
-
-  //             }
-  //             Serial.println();
-  //             Serial.print("New setpoint for wrist pitch: ");Serial.println(Wrist_Pitch.getSetpoint());
-  //             valid_input = true;
-  //             break;
-            
-  //           case 'S':
-  //             Serial.print("Current encoder position for wrist pitch: ");
-  //             Serial.println(Wrist_Pitch.getCurrentAngle());
-  //             delay(200);
-  //             valid_input = true;
-  //             break;
-            
-  //           case 'D':
-  //             Serial.print("Enter new setpoint for wrist roll: ");
-  //             while(true){
-  //               if(Serial.available()){
-  //                 double set = (double) Serial.parseFloat();
-  //               Wrist_Roll.newSetpoint(set); 
-  //                 delay(1000);
-  //                 break;
-  //               }
-
-
-  //             }
-  //             Serial.println();
-  //             Serial.print("New setpoint for wrist roll: ");Serial.println(Wrist_Roll.getSetpoint());
-  //             valid_input = true;
-  //             break;
-
-  //           case 'F':
-  //             Serial.print("Current encoder position for wrist roll: ");
-  //             Serial.println(Wrist_Roll.getCurrentAngle());
-  //             delay(200);
-  //             valid_input = true;
-  //             break;
-            
-  //           case 'G':
-  //             Serial.print("Enter new setpoint for shoulder: ");
-  //             while(true){
-  //               if(Serial.available()){
-  //                 double set = (double) Serial.parseFloat();
-  //                 Shoulder.newSetpoint(set); 
-  //                 delay(1000);
-  //                 break;
-  //               }
-
-
-  //             }
-  //             Serial.println();
-  //             Serial.print("New setpoint for shoulder: ");Serial.println(Shoulder.getSetpoint());
-  //             valid_input = true;
-  //             break;
-
-  //           case 'H':
-  //             Serial.print("Current encoder position for wrist roll: ");
-  //             Serial.println(Shoulder.getCurrentAngle());
-  //             delay(200);
-  //             valid_input = true;
-  //             break;
-            
-  //           case 'J':
-  //             Serial.print("Enter new setpoint for elbow: ");
-  //             while(true){
-  //               if(Serial.available()){
-  //                 double set = (double) Serial.parseFloat();
-  //                 Elbow.newSetpoint(set); 
-  //                 delay(1000);
-  //                 break;
-  //               }
-
-
-  //             }
-  //             Serial.println();
-  //             Serial.print("New setpoint for elbow: ");Serial.println(Elbow.getSetpoint());
-  //             valid_input = true;
-  //             break;
-
-  //           case 'K':
-  //             Serial.print("Current encoder position for elbow: ");
-  //             Serial.println(Elbow.getCurrentAngle());
-  //             delay(200);
-  //             valid_input = true;
-  //             break;
-
-  //           case 'W':
-  //             Serial.print("Enter new setpoint for waist: ");
-  //             while(true){
-  //               if(Serial.available()){
-  //                 double set = (double) Serial.parseFloat();
-  //                 Waist.newSetpoint(set); 
-  //                 delay(1000);
-  //                 break;
-  //               }
-
-
-  //             }
-  //             Serial.println();
-  //             Serial.print("New setpoint for waist: ");Serial.println(Waist.getSetpoint());
-  //             valid_input = true;
-  //             break;
-
-  //           case 'E':
-  //             Serial.print("Current encoder position for waist: ");
-  //             Serial.println(Waist.getCurrentAngle());
-  //             delay(200);
-  //             valid_input = true;
-  //             break;
-  //         }
-  //       }
-  //     } 
-  //   }
-  // }
-
-  // Reset timer here so .tick() can run
 
   if(SerialAPI::update()){
     memset(buffer, 0, SERIAL_RX_BUFFER_SIZE);
@@ -294,17 +146,24 @@ void loop() {
     wp_position = positions[1]; 
     shldr_position = positions[2];
     elbw_position = positions[3];
-    waist_position = positions[4]; 
+    waist_position = positions[4];
+  
 
-    delay(50); 
+    //memcpy(&tempClaw, buffer+21, 4);
+    //clawState = (tempClaw > 0) ? 1.0 : 0.0;
+    
 
-    buf[0] = '1';
-    memcpy(buf+1, positions, 20); 
-    SerialAPI::send_bytes('0', buf, 21);
     delay(100); 
+
+    buf[0] = '0';
+    memcpy(buf+1, positions, 24);
+    //memcpy(buf+21, *clawState, 4);
+    SerialAPI::send_bytes('0', buf, 25);
+    delay(100);
 
     //TODO: add code to send limit switch info
   }
+  SerialAPI::send_bytes('0', temp_msg, 25);
   Wrist_Roll.newSetpoint((double) wr_position);
   Wrist_Roll.tick();
 
@@ -326,8 +185,8 @@ void loop() {
 
 // If the connection is lost and .tick() is blocked from running for more than 10ms, 
 // disable motor until the error is recovered from
-void ConnectionLostISR(){
-  WatchdogIntClear(WATCHDOG0_BASE);
-  analogWrite(WR_PWM, 0);
-  analogWrite(WP_PWM, 0);
-}
+// void ConnectionLostISR(){
+//   WatchdogIntClear(WATCHDOG0_BASE);
+//   analogWrite(WR_PWM, 0);
+//   analogWrite(WP_PWM, 0);
+// }
